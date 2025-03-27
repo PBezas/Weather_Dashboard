dae7441950a127697c01d8118062af1851af29eb
Panos Bezas <p.bezas1@gmail.com>
2025-03-27 22:19:10 +0200

    Add styles to App container

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import UserPrompt from "./components/UserPrompt";
+import WeatherDisplay from "./components/WeatherDisplay";
+
 function App() {
-  return <div>Weather Dashboard</div>;
+  return (
+    <div className="bg-emerald-300 flex flex-col justify-start items-center h-[100vh]">
+      <h1 className="m-3 text-4xl text-white font-bold">Wheather Dashboard</h1>
+      <div className="flex flex-col w-[50%] justify-center items-center">
+        <UserPrompt />
+        <WeatherDisplay />
+      </div>
+    </div>
+  );
 }
 
 export default App;
