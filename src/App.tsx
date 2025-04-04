0ef37c454b88198b1cb4aa0e39f3cafdae90871a
Panos Bezas <p.bezas1@gmail.com>
2025-04-04 16:04:58 +0300

    implement error handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 import UserPrompt from "./components/UserPrompt";
 import WeatherDisplay from "./components/WeatherDisplay";
 import sendPrompt from "./api";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { WeatherDataType } from "./types/types";
+import { AxiosError } from "axios";
 
 function App() {
   const [data, setData] = useState<WeatherDataType | null>(null);
+  const [error, setError] = useState("");
 
   const sumbitPrompt = async (prompt: string) => {
-    const weatherData = await sendPrompt(prompt);
-    setData(weatherData);
+    try {
+      const weatherData = await sendPrompt(prompt);
+      setData(weatherData);
+    } catch (err) {
+      setError((err as AxiosError)?.response?.data as string);
+      throw err;
+    }
   };
+
+  useEffect(() => {
+    const timeId = setTimeout(() => setError(""), 2000);
+    return () => clearTimeout(timeId);
+  }, [error]);
 
   return (
     <div className="bg-emerald-300 flex flex-col justify-start items-center h-[100vh]">
@@ -18,6 +30,11 @@ function App() {
       <div className="flex flex-col w-[50%] justify-center items-center">
         <UserPrompt sumbitPrompt={sumbitPrompt} />
         <WeatherDisplay weatherData={data as unknown as WeatherDataType} />
+        {error && (
+          <div className="text-white font-bold mt-10 bg-red-400 p-4 rounded-2xl">
+            {error}
+          </div>
+        )}
       </div>
     </div>
   );
